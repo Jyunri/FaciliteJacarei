ec4f38df9e3b3dd82b8f88b6c0faab9680ebb9e3
Jyunri <nekojimy@gmail.com>
2016-05-29 18:19:49 -0300

    remove title

--- app/src/main/java/br/com/cdf/facilitejacarei/GuiaActivity.java
+++ app/src/main/java/br/com/cdf/facilitejacarei/GuiaActivity.java
@@ -11,6 +11,7 @@ public class GuiaActivity extends AppCompatActivity implements View.OnClickListe
     Button btMapa, btLista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guia);
 
